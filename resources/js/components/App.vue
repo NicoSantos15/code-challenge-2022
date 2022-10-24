b5227082724401599df6d50d4f9614289194100d
Nico Santos <nicos@citb.org.au>
2022-10-24 17:17:24 +1030

    Trying to send JSON file straight from controller.

---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Verto Group - Unsplash CRUD</h1>
+  <p>{{ users }}</p>
+</template>
+
+<script>
+export default {
+  props: ['users'],
+//   data: function () {
+//     return { hashid: "" };
+//   },
+};
+</script>
---
+++ resources/views/search.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>How To Install Vue 3 in Laravel 9 with Vite</title>
+</head>
+
+<body>
+    <div id="app">
+		<span></span>
+	</div>
+    @vite('resources/js/app.js')
+</body>
+
+</html>
+
+{{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
